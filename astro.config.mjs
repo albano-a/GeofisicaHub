10961c144c364b0997147e0d2b7edce5c0769377
André Luiz Albano <110417140+albano-a@users.noreply.github.com>
2024-09-09 18:32:35 -0300

    fix: Fixed Google Analytics?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import partytown from "@astrojs/partytown";
 
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://geofisicahub.live/',
-  integrations: [preact()]
+  site: "https://geofisicahub.live/",
+  integrations: [
+    preact(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
